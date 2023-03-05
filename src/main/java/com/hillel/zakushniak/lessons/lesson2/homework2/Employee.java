df3969efb68a5cd8556f8ebf23cbf528e8597bcc
Zakushniak Sergii <ok.sergey@gmail.com>
2023-03-05 19:55:24 +0200

    homework2

---
+++ src/main/java/com/hillel/zakushniak/lessons/lesson2/homework2/Employee.java
@@ -0,0 +1,17 @@
+package main.java.com.hillel.zakushniak.lessons.lesson2.homework2;
+
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private String age;
+
+    public Employee(String name, String position, String email, String phone, String age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+    }
+}
