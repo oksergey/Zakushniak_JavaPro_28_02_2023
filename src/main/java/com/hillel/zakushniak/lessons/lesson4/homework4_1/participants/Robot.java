76fb56227c55fb65d08ca9cc669d353a47fa71ea
Zakushniak Sergii <ok.sergey@gmail.com>
2023-03-15 09:15:59 +0200

    homework4 task with * solved

--- src/main/java/com/hillel/zakushniak/lessons/lesson4/homework4_1/participants/Robot.java
+++ src/main/java/com/hillel/zakushniak/lessons/lesson4/homework4_1/participants/Robot.java
@@ -12,11 +12,11 @@ public class Robot extends Participant {
 
     @Override
     public void jump() {
-        System.out.println("Participant robot " + getName() + " jumped obstacle WALL with Height " + Wall.height);
+        System.out.println(getName() + " jumped obstacle WALL with Height " + Wall.height);
     }
 
     @Override
     public void run() {
-        System.out.println("Participant robot " + getName() + " runned obstacle RunningTrack with Distance " + RunningTrack.length);
+        System.out.println(getName() + " runned obstacle RunningTrack with Distance " + RunningTrack.length);
     }
 }
