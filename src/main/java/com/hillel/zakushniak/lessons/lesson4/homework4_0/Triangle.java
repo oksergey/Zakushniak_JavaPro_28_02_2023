1163aaace096e66bc82fff3c7e85113a4438419d
Zakushniak Sergii <ok.sergey@gmail.com>
2023-03-22 22:02:44 +0200

    changed gitignore

---
+++ src/main/java/com/hillel/zakushniak/lessons/lesson4/homework4_0/Triangle.java
@@ -0,0 +1,17 @@
+package main.java.com.hillel.zakushniak.lessons.lesson4.homework4_0;
+
+public class Triangle implements GeometricFigure {
+
+    private double heightOfTriangle;
+    private double sideOfTriangle;
+
+    public Triangle(double heightOfTriangle, double sideOfTriangle) {
+        this.heightOfTriangle = heightOfTriangle;
+        this.sideOfTriangle = sideOfTriangle;
+    }
+
+    @Override
+    public double getSquare() {
+        return heightOfTriangle * sideOfTriangle / 2;
+    }
+}
