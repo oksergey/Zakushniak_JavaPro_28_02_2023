51b4ec5396b3e5769a72648c33181ec0756b2172
Zakushniak Sergii <ok.sergey@gmail.com>
2023-03-22 21:43:58 +0200

    homework5

---
+++ src/main/java/com/hillel/zakushniak/lessons/lesson6/homework5/Animal.java
@@ -0,0 +1,20 @@
+package main.java.com.hillel.zakushniak.lessons.lesson6.homework5;
+
+public class Animal {
+
+    private String name;
+    private int occurrence;
+
+    public Animal(String name, int occurrence) {
+        this.name = name;
+        this.occurrence = occurrence;
+    }
+
+
+    @Override
+    public String toString() {
+        return "\n " +
+                "name='" + name + '\'' +
+                ", occurrence=" + occurrence;
+    }
+}
