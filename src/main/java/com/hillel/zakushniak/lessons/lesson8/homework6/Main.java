85eb4cb77da847a36f01f7c9d87951755d2b79a9
Zakushniak Sergii <ok.sergey@gmail.com>
2023-03-29 20:21:34 +0300

    HomeWork6

---
+++ src/main/java/com/hillel/zakushniak/lessons/lesson8/homework6/Main.java
@@ -0,0 +1,148 @@
+package main.java.com.hillel.zakushniak.lessons.lesson8.homework6;
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--------Task #1");
+        System.out.println("Is the string palindrome?");
+
+        System.out.print('\"' + "A man, a plan, a canal: Panama" + '\"' + '\t');
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+
+        System.out.print('\"' + "race a car" + '\"' + '\t');
+        System.out.println(isPalindrome("race a car"));
+
+        System.out.print('\"' + "" + '\"' + '\t');
+        System.out.println(isPalindrome(" "));
+
+        System.out.println("--------Task #2");
+        int[] nums1 = new int[]{1, 2, 3, 1};
+        System.out.println(Arrays.toString(nums1));
+        System.out.println(isElementsDistinct(nums1));
+
+        int[] nums2 = new int[]{1, 2, 3, 4};
+        System.out.println(Arrays.toString(nums2));
+        System.out.println(isElementsDistinct(nums2));
+
+        int[] nums3 = new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        System.out.println(Arrays.toString(nums3));
+        System.out.println(isElementsDistinct(nums3));
+
+        System.out.println("--------Task #3");
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(nums) + " target = " + target);
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+
+        nums = new int[]{3, 2, 4};
+        target = 6;
+        System.out.println(Arrays.toString(nums) + " target = " + target);
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+
+        nums = new int[]{3, 3};
+        target = 6;
+        System.out.println(Arrays.toString(nums) + " target = " + target);
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+
+//        System.out.println(Arrays.toString(twoSum(new int[]{3, 2, 4}, 6)));
+//        twoSum(new int[]{3, 2, 4}, 6);
+//        twoSum(new int[]{3, 3}, 6);
+
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] numsCloned = nums.clone();
+
+        Arrays.sort(numsCloned);
+        int i = 0;
+        int j = numsCloned.length - 1;
+
+        for (; i != j; ) {
+            if ((numsCloned[i] + numsCloned[j]) == target) {
+//                System.out.println("solution: " + i + " " + j);
+                break;
+            }
+            for (; j != i; ) {
+                if ((numsCloned[i] + numsCloned[j]) == target) {
+                    break;
+                }
+                if ((numsCloned[i] + numsCloned[j]) > target) {
+                    j--;
+                } else i++;
+            }
+        }
+        for (int k = 0; k < nums.length; k++) {
+            if (nums[k] == numsCloned[i]) {
+                i = k;
+                break;
+            }
+        }
+        for (int k = 0; k < nums.length; k++) {
+            if (nums[k] == numsCloned[j] && k != i) {
+                j = k;
+                break;
+            }
+        }
+        return new int[]{i, j};
+    }
+
+    public static boolean isElementsDistinct(int[] nums) {
+        Set<Integer> withoutDistincts = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (withoutDistincts.contains(nums[i])) {
+                return true;
+            } else {
+                withoutDistincts.add(nums[i]);
+            }
+        }
+        return false;
+    }
+
+    //Time Limit Exceede
+//    public static boolean isElementsDistinct(int[] nums) {
+//        List<Integer> withoutDistincts = new ArrayList<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            if (withoutDistincts.contains(nums[i])) {
+//                return true;
+//            } else {
+//                withoutDistincts.add(nums[i]);
+//            }
+//        }
+//        return false;
+//    }
+
+
+    //Time Limit Exceede
+//    public static boolean isElementsDistinct(int[] nums) {
+//        for (int i = 0; i < nums.length; i++) {
+//            int counter = 0;
+//            for (int j = 0; j < nums.length; j++) {
+//                if (nums[i] == nums[j]) counter++;
+//                if (counter > 1) return true;
+//            }
+//        }
+//        return false;
+//    }
+
+    public static boolean isPalindrome(String str) {
+
+        String inLowerCase = str.toLowerCase();
+        StringBuilder alphanumeric = new StringBuilder();
+        StringBuilder alphanumericReversed = new StringBuilder();
+
+        for (int i = 0; i < inLowerCase.length(); i++) {
+            char c = inLowerCase.charAt(i);
+            if (Character.isDigit(c) || Character.isLetter(c)) {
+                alphanumeric.append(c);
+            }
+        }
+        for (int i = alphanumeric.length() - 1; i >= 0; i--) {
+            alphanumericReversed.append(alphanumeric.charAt(i));
+        }
+
+        return alphanumeric.toString().equals(alphanumericReversed.toString());
+    }
+}
