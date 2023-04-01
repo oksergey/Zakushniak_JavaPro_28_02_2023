80ea2d5973133dd5588ba807c67aea6372f6ff59
Zakushniak Sergii <ok.sergey@gmail.com>
2023-04-01 22:59:23 +0300

    HomeWork7

---
+++ src/main/java/com/hillel/zakushniak/lessons/lesson9/Order.java
@@ -0,0 +1,30 @@
+package main.java.com.hillel.zakushniak.lessons.lesson9;
+
+public class Order {
+
+    private int orderNumber;
+    private final String name;
+
+    public Order(int orderNumber, String name) {
+        this.orderNumber = orderNumber;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + orderNumber + " | " + name;
+    }
+
+}
