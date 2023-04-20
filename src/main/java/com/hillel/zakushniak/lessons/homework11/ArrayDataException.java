e9c2898afa4866fed294633d6443b1ef4332a575
Zakushniak Sergii <ok.sergey@gmail.com>
2023-04-20 21:39:28 +0300

    homeWork11 (exceptions)

---
+++ src/main/java/com/hillel/zakushniak/lessons/homework11/ArrayDataException.java
@@ -0,0 +1,8 @@
+package main.java.com.hillel.zakushniak.lessons.homework11;
+
+public class ArrayDataException extends RuntimeException {
+
+    public ArrayDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/hillel/zakushniak/lessons/homework11/ArrayValueCalculator.java
@@ -0,0 +1,29 @@
+package main.java.com.hillel.zakushniak.lessons.homework11;
+
+public class ArrayValueCalculator {
+
+    static int doCalc(String[][] array) throws ArraySizeException {
+        int sum = 0;
+
+        if (array.length == 4 && array[0].length == 4) {
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    try {
+                        sum += Integer.parseInt(array[i][j]);
+                    } catch (NumberFormatException e) {
+                        try {
+                            throw new ArrayDataException("Cell [" + i + "][" + j + "] is ignored as don`t contain Int!");
+                        } catch (ArrayDataException ex) {
+                            System.err.println(ex);
+                        }
+                    }
+                }
+            }
+
+        } else {
+            throw new ArraySizeException("Wrong array size!");
+        }
+
+        return sum;
+    }
+}
